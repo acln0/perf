4ea196af196042469af42d0d9f7451da512d3d79
Andrei Tudor Călin <mail@acln.ro>
2019-03-16 10:53:26 +0100

    enhance examples, move them next to the relevant types
    Signed-off-by: Andrei Tudor Călin <mail@acln.ro>

--- perf_example_test.go
+++ perf_example_test.go
@@ -47,25 +47,53 @@ func ExampleHardwareCounter_iPC() {
 		sum, gc.Running, insns, cycles, float64(insns)/float64(cycles))
 }
 
-func ExampleEvent_Measure_tracepoint() {
-	getpidattr := new(perf.Attr)
-	getpid := perf.Tracepoint("syscalls", "sys_enter_getpid")
-	if err := getpid.Configure(getpidattr); err != nil {
+func ExampleSoftwareCounter_pageFaults() {
+	pfa := new(perf.Attr)
+	perf.PageFaults.Configure(pfa)
+
+	runtime.LockOSThread()
+	defer runtime.UnlockOSThread()
+
+	faults, err := perf.Open(pfa, perf.CallingThread, perf.AnyCPU, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer faults.Close()
+
+	var mem []byte
+	const (
+		size = 64 * 1024 * 1024
+		pos  = 63 * 1024 * 1024
+	)
+	c, err := faults.Measure(func() {
+		mem = make([]byte, size)
+		mem[pos] = 42
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("saw %d page faults, wrote value %d", c.Value, mem[pos])
+}
+
+func ExampleTracepoint_getpid() {
+	ga := new(perf.Attr)
+	gtp := perf.Tracepoint("syscalls", "sys_enter_getpid")
+	if err := gtp.Configure(ga); err != nil {
 		log.Fatal(err)
 	}
 
 	runtime.LockOSThread()
 	defer runtime.UnlockOSThread()
 
-	ev, err := perf.Open(getpidattr, perf.CallingThread, perf.AnyCPU, nil)
+	getpid, err := perf.Open(ga, perf.CallingThread, perf.AnyCPU, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer ev.Close()
+	defer getpid.Close()
 
 	unix.Getpid() // does not count towards the measurement
 
-	c, err := ev.Measure(func() {
+	c, err := getpid.Measure(func() {
 		unix.Getpid()
 		unix.Getpid()
 		unix.Getpid()
@@ -74,34 +102,6 @@ func ExampleEvent_Measure_tracepoint() {
 		log.Fatal(err)
 	}
 
-	fmt.Printf("saw getpid(2) %d times", c.Value) // should print 3
-}
-
-func ExampleEvent_MeasureGroup_hardware() {
-	var g perf.Group
-	g.Add(perf.Instructions, perf.CPUCycles)
-
-	runtime.LockOSThread()
-	defer runtime.UnlockOSThread()
-
-	ev, err := g.Open(perf.CallingThread, perf.AnyCPU)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer ev.Close()
-
-	sum := 0
-
-	gc, err := ev.MeasureGroup(func() {
-		for i := 0; i < 1000000; i++ {
-			sum += i
-		}
-	})
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	insns := gc.Values[0].Value
-	cycles := gc.Values[1].Value
-	fmt.Printf("got sum %d in %d instructions and %d CPU cycles", sum, insns, cycles)
+	fmt.Printf("saw getpid(2) %d times\n", c.Value)
+	// Output: saw getpid(2) 3 times
 }
