e24ee44d49da2e94ada7b8eb3b313dd9bfd7533c
Peter Waller <p@pwaller.net>
2020-05-10 13:45:26 +0100

    Use paranoid(0) for testCPUWideSwitch
    Fixes: #15

--- record_test.go
+++ record_test.go
@@ -775,7 +775,7 @@ func testExit(t *testing.T) {
 }
 
 func testCPUWideSwitch(t *testing.T) {
-	requires(t, paranoid(1), softwarePMU)
+	requires(t, paranoid(0), softwarePMU)
 
 	var wg sync.WaitGroup
 	ready := make(chan error)
